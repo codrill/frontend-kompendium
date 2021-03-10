42a5b1c49d4bf7d3859d0028d0591636a7a5d8d6
Radomir Skrzepij <zyziu10@gmail.com>
2021-03-10 11:28:14 +0100

    Add SCSS support (#3)
    * install sass and refactor existing files
    
    * remove unused classes from html markup
    ^FEK-11 done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app'
 
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
