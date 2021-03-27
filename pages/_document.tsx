c5ed2c26f96d0b41c7db98a1e0dcf109c9484c11
Radomir Skrzepij <zyziu10@gmail.com>
2021-03-27 17:07:01 +0100

    [FEK-21] Connect with google-analytics using gtags (#19)
    * Add google analytics through gtag.js
    * linter fix - cleanup imports in `_document.tsx`
    
    ^FEK-21 done

---
+++ pages/_document.tsx
@@ -0,0 +1,47 @@
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+import { ReactFragment } from 'react'
+
+import { GA_TRACKING_ID } from '../lib/gtag'
+
+type GetInitialPropsResponse = {
+  html: string
+  head?: JSX.Element[]
+  styles?: ReactFragment
+}
+
+class FEKDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<GetInitialPropsResponse> {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html lang="pl-PL">
+        <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default FEKDocument
