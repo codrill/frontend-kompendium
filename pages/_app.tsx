babaa6d17d55bd711171a06d2563438a6c269928
Radomir Skrzepij <zyziu10@gmail.com>
2021-04-19 08:56:21 +0200

    next-SEO plugin configuration (#28)
    ^FEK-20 done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
+
+import { SEO } from '../next-seo.config'
 
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
