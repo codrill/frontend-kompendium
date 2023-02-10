0a091be3178c07821bc033a3b14019c46fb77682
Radomir Skrzepij <zyziu10@gmail.com>
2023-02-10 22:13:19 +0100

    46 update nextjs dependency to v13x (#47)
    * update required deps
    
    * auto-update for tsconfig
    
    * fix tocItem to work well with next.js v13 api
    
    ---------
    
    Co-authored-by: Radomir Skrzepij <radomir.skrzepij@amano.ai>

--- components/tocItem/tocItem.tsx
+++ components/tocItem/tocItem.tsx
@@ -9,10 +9,7 @@ export function TocItem({ anchor, title }: TocItemProps): JSX.Element {
   return (
     <li>
       <Link href={`#${anchor}`} shallow={true}>
-        {/*Turn off checking required anchor attributes due to Next API*/}
-        {/*Known Issue: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md#case-i-use-nextjs-and-im-getting-this-error-inside-of-links*/}
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a>{title}</a>
+        {title}
       </Link>
     </li>
   )
