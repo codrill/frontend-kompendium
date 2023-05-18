4c3c615aa61593fe315464f49cdad9ddba5be680
Radomir Skrzepij <zyziu10@gmail.com>
2023-05-18 21:17:37 +0200

    Feature/setup firebase hosting (#55)
    * setup firebase hosting
    
    * remove unused hello.ts and add firebase cache folder to gitignore
    
    * remove old analytics definition from document,tsx
    
    * add .env variables with config for firebase
    
    * install firebase dependency
    
    * init firebase analytics config - only for production build
    
    * firebase.ts config file
    
    * add next.config with definition defined output
    this way `build` will automatically export static files
    
    ---------
    
    Co-authored-by: Radomir Skrzepij <radomir.skrzepij@amano.ai>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics, isSupported } from 'firebase/analytics'
 
+import { firebaseConfig } from '../firebase'
 import { SEO } from '../next-seo.config'
 
 import '../styles/globals.scss'
+
+if (process.env.NEXT_PUBLIC_ANALYTICS === 'enabled') {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const analytics = isSupported().then((yes) => (yes ? getAnalytics(app) : null))
+}
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
